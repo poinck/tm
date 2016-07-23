a9fb6ae55667a98f9b75bd783362dfc721d26d60
André <aklausni@gmail.com>
2016-07-24 00:59:18 +0200

    add local deployment

--- tm/web/deploy.sh
+++ tm/web/deploy.sh
@@ -1,16 +1,13 @@
 #!/bin/bash
+# this script is used for development; adjust to your needs
 
-./get_tmcsv.sh
-if [[ "$?" -gt 0 ]] ; then
-    echo "ERROR  no 'tm_sum.csv'; the deployt tm-web will"
-    echo "       show no temperature data"
-    echo "       'get_tmcsv.sh' is not part of tm."
+if [[ "$1" == "local" ]] ; then
+    # fetch csv from original location to prevent cross-origin-problem.
+    ./get_tmcsv.sh
 fi
 
 cp /home/poinck/gits/tm/tm/web/* /var/www/localhost/htdocs/.
 
-chmod a+r /var/www/localhost/htdocs/farbbalken.csv
-chmod a+r /var/www/localhost/htdocs/d3workshop.html
 chmod a+r /var/www/localhost/htdocs/index.html
 chmod a+r /var/www/localhost/htdocs/tmconfig.js
 chmod a+r /var/www/localhost/htdocs/tm.js
