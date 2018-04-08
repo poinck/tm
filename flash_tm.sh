d72b3889785dfa6943071d5ea49b731c3479242c
André <andre@poinck.de>
2018-04-08 18:55:50 +0200

    flashing: support different USB ports

--- flash_tm.sh
+++ flash_tm.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+DEFAULT_PORT=0
+port="$1"
+if [[ -z "port" ]] ; then
+    port=${DEFAULT_PORT}
+fi
+
 set -x
-arduino --upload tm.ino --port /dev/ttyACM0 && screen /dev/ttyACM0 9600
+arduino --upload tm.ino --port /dev/ttyACM${port} && screen /dev/ttyACM${port} 9600
 #set -x
